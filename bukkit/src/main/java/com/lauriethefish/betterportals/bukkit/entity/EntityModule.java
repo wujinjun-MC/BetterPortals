ea3ee34ff1d0f0f968afe9241f01528c11de2abc
Lauriethefish <lauriethefish@outlook.com>
2021-11-19 16:08:50 +0000

    Switch back to regular entity tracker for now

--- bukkit/src/main/java/com/lauriethefish/betterportals/bukkit/entity/EntityModule.java
+++ bukkit/src/main/java/com/lauriethefish/betterportals/bukkit/entity/EntityModule.java
@@ -8,23 +8,12 @@ import com.lauriethefish.betterportals.bukkit.nms.NMSEntityTracker;
 public class EntityModule extends AbstractModule {
     @Override
     public void configure() {
-        /*
         install(new FactoryModuleBuilder()
                 .implement(IEntityTracker.class, EntityTracker.class)
                 .build(IEntityTracker.Factory.class)
         );
 
         bind(IEntityPacketManipulator.class).to(EntityPacketManipulator.class);
-        bind(EntityTrackingManager.class).to(EventEntityTrackingManager.class);*/
-
-        // TODO: Enable this only when on correct version with a manual override in the config
-        install(new FactoryModuleBuilder()
-                .implement(IEntityTracker.class, NMSEntityTracker.class)
-                .build(IEntityTracker.Factory.class)
-        );
-
-        bind(IEntityPacketManipulator.class).to(EntityPacketManipulator.class);
-        bind(EntityTrackingManager.class).to(NoUpdateEntityTrackingManager.class);
-
+        bind(EntityTrackingManager.class).to(EventEntityTrackingManager.class);
     }
 }
