b9596deb62a7efe644b19e489962e802b658a6b4
Lauriethefish <lauriethefish@outlook.com>
2021-12-12 18:48:09 +0000

    Add portal server bootstrap Also added server switch to sync selections

--- velocity/src/main/java/com/lauriethefish/betterportals/velocity/BetterPortals.java
+++ velocity/src/main/java/com/lauriethefish/betterportals/velocity/BetterPortals.java
@@ -1,14 +1,96 @@
 package com.lauriethefish.betterportals.velocity;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.lauriethefish.betterportals.proxy.net.IClientHandler;
+import com.lauriethefish.betterportals.proxy.net.IPortalServer;
+import com.lauriethefish.betterportals.shared.net.RequestException;
+import com.lauriethefish.betterportals.shared.net.requests.PreviousServerPutRequest;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.player.ServerConnectedEvent;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.event.proxy.ProxyShutdownEvent;
 import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.proxy.ProxyServer;
 import org.slf4j.Logger;
 
 import javax.inject.Inject;
 
 @Plugin(id = "better-portals", name = "BetterPortals", version = "0.10.0", authors = "lauriethefish@outlook.com")
 public class BetterPortals {
+    private final Injector injector;
+    private final Logger logger;
+    private IPortalServer portalServer;
+
     @Inject
-    public BetterPortals(Logger logger) {
-        logger.info("BetterPortals loaded!");
+    public BetterPortals(MainModule mainModule, Logger logger) {
+        this.injector = Guice.createInjector(mainModule);
+        this.logger = logger;
+    }
+
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent event) {
+        try {
+            injector.getInstance(Config.class).load();
+        } catch (Exception ex) {
+            logger.error("Failed to load config file", ex);
+            return;
+        }
+
+        try {
+            this.portalServer = injector.getInstance(IPortalServer.class);
+            portalServer.startUp();
+        }   catch(RuntimeException ex) {
+            logger.error("Failed to start up portal server", ex);
+        }
+    }
+
+    @Subscribe
+    public void onProxyShutdown(ProxyShutdownEvent event) {
+        if(portalServer != null) {
+            try {
+                portalServer.shutDown();
+            }   catch(RuntimeException ex) {
+                logger.error("Failed to shut down portal server", ex);
+            }
+        }
+    }
+
+    @Subscribe
+    public void onServerSwitch(ServerConnectedEvent event) {
+        if(portalServer == null) { return; }
+
+        if(!event.getPreviousServer().isPresent()) {
+            return; // We only care about changes between two servers for selection copying
+        }
+
+        IClientHandler to = portalServer.getServer(event.getServer().getServerInfo().getName());
+
+        String previousServerName = event.getPreviousServer().get().getServerInfo().getName();
+        IClientHandler from = portalServer.getServer(event.getServer().getServerInfo().getName());
+
+        if(from == null) {
+            logger.debug("From server was unregistered for server switch event, skipping");
+            return;
+        }
+
+        if(to == null) {
+            logger.debug("To server was unregistered for server switch event, skipping");
+            return;
+        }
+
+        logger.debug("Sending previous server put request");
+        PreviousServerPutRequest request = new PreviousServerPutRequest();
+        request.setPlayerId(event.getPlayer().getUniqueId());
+        request.setPreviousServer(previousServerName);
+
+        to.sendRequest(request, (response) -> {
+            try {
+                response.checkForErrors();
+            }   catch(RequestException ex) {
+                logger.warn("Failed to set previous server for player {}", event.getPlayer().getUniqueId());
+                ex.printStackTrace();
+            }
+        });
     }
 }
---
+++ velocity/src/main/java/com/lauriethefish/betterportals/velocity/MainModule.java
@@ -0,0 +1,47 @@
+package com.lauriethefish.betterportals.velocity;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.name.Names;
+import com.lauriethefish.betterportals.proxy.IProxy;
+import com.lauriethefish.betterportals.proxy.IProxyConfig;
+import com.lauriethefish.betterportals.proxy.net.ProxyModule;
+import com.lauriethefish.betterportals.shared.logging.Logger;
+import com.velocitypowered.api.plugin.PluginContainer;
+import com.velocitypowered.api.plugin.annotation.DataDirectory;
+import com.velocitypowered.api.proxy.ProxyServer;
+
+import javax.inject.Inject;
+import java.nio.file.Path;
+import java.util.Optional;
+
+public class MainModule extends AbstractModule {
+    private final ProxyServer proxyServer;
+    private final Path dataDirectory;
+    private final org.slf4j.Logger logger;
+
+    private final String pluginVersion;
+
+    @Inject
+    public MainModule(ProxyServer proxyServer, @DataDirectory Path dataDirectory, PluginContainer pluginContainer, org.slf4j.Logger logger) {
+        this.proxyServer = proxyServer;
+        this.dataDirectory = dataDirectory;
+        this.logger = logger;
+
+        Optional<String> version = pluginContainer.getDescription().getVersion();
+        if(!version.isPresent()) {
+            throw new IllegalStateException("Plugin had no version");
+        }
+        this.pluginVersion = version.get();
+    }
+
+    protected void configure() {
+        install(new ProxyModule());
+
+        bind(IProxyConfig.class).to(Config.class);
+        bind(ProxyServer.class).toInstance(proxyServer);
+        bind(IProxy.class).to(VelocityProxy.class);
+        bind(String.class).annotatedWith(Names.named("pluginVersion")).toInstance(pluginVersion);
+        bind(Path.class).annotatedWith(Names.named("dataDirectory")).toInstance(dataDirectory);
+        bind(Logger.class).toInstance(new VelocityLogger(logger));
+    }
+}
