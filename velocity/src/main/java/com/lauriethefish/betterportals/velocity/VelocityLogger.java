29297463dfb9986975f0326a9f38b7c663e633be
Lauriethefish <lauriethefish@outlook.com>
2021-12-15 16:03:54 +0000

    Replicate log levels to slf4j levels (rough match)

--- velocity/src/main/java/com/lauriethefish/betterportals/velocity/VelocityLogger.java
+++ velocity/src/main/java/com/lauriethefish/betterportals/velocity/VelocityLogger.java
@@ -20,6 +20,20 @@ public class VelocityLogger extends Logger {
         // Return if the log level is too low
         if(record.getLevel().intValue() < super.getLevel().intValue()) {return;}
 
-        logger.info(record.getMessage());
+        // Approximately convert between log4j and java.util logging levels
+        int recordLevel = record.getLevel().intValue();
+        if(recordLevel >= Level.SEVERE.intValue()) {
+            logger.error("{}", record.getMessage());
+        }   else if(recordLevel >= Level.WARNING.intValue()) {
+            logger.warn("{}", record.getMessage());
+        }   else if(recordLevel >= Level.INFO.intValue()) {
+            logger.info("{}", record.getMessage());
+        }   else if(recordLevel >= Level.FINE.intValue()) {
+            logger.info("[FNE] {}", record.getMessage());
+        }   else if(recordLevel >= Level.FINER.intValue()) {
+            logger.info("[FNR] {}", record.getMessage());
+        }   else if(recordLevel >= Level.FINEST.intValue()) {
+            logger.info("[FST] {}", record.getMessage());
+        }
     }
 }
