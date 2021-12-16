02d1c004249c0d3e89663eb67a51ed1deabb851d
Lauriethefish <lauriethefish@outlook.com>
2021-12-16 18:48:46 +0000

    Change light level depending on world and time of day

--- bukkit/src/main/java/com/lauriethefish/betterportals/bukkit/block/lighting/LightDataManger.java
+++ bukkit/src/main/java/com/lauriethefish/betterportals/bukkit/block/lighting/LightDataManger.java
@@ -4,6 +4,7 @@ import com.comphenix.protocol.wrappers.WrappedBlockData;
 import com.lauriethefish.betterportals.bukkit.portal.IPortal;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.block.data.type.Light;
 import org.jetbrains.annotations.Nullable;
 
@@ -11,11 +12,42 @@ import javax.inject.Singleton;
 
 @Singleton
 public class LightDataManger implements ILightDataManager   {
+    private int getLightLevel(IPortal portal) {
+        World destWorld = portal.getDestPos().getWorld();
+        if(destWorld == null) {
+            return -1;
+        }
+
+        World.Environment destEnv = destWorld.getEnvironment();
+        if(destEnv == World.Environment.NETHER) {
+            return 8; // Set a dim light by default in the nether (avoids nether in caves showing as too dark)
+        }   else if(destEnv == World.Environment.NORMAL) {
+            // If close-ish to the surface
+            if(portal.getDestPos().getVector().getY() > 64.0) {
+                long time = destWorld.getTime();
+                if (time > 0 && time < 12300) {
+                    return 15; // Set to 15 during day time
+                } else {
+                    return -1; // Disable lighting during night time
+                }
+            }   else    {
+                return -1; // No lighting needed in caves
+            }
+        }
+
+        return -1;
+    }
+
     @Override
     public @Nullable WrappedBlockData getLightData(IPortal portal) {
-        // TODO: Automatically determine light level depending on dimension, Y level, surface light, etc.
-        Light lightBlockData = (Light) Bukkit.createBlockData(Material.LIGHT);
-        lightBlockData.setLevel(10);
-        return WrappedBlockData.createData(lightBlockData);
+        int lightLevel = getLightLevel(portal);
+
+        if(lightLevel == -1) {
+            return null;
+        }   else    {
+            Light lightBlockData = (Light) Bukkit.createBlockData(Material.LIGHT);
+            lightBlockData.setLevel(lightLevel);
+            return WrappedBlockData.createData(lightBlockData);
+        }
     }
 }
